485ed0b93284223b0f777f50fc74b18538296579
Spiegelin <100853421+Spiegelin@users.noreply.github.com>
2023-03-12 12:16:23 -0600

    Bluetooth

--- Shared/Bluetooth Desvergue.swift
+++ Shared/Bluetooth Desvergue.swift
@@ -21,14 +21,16 @@ class BluetoothViewModel: NSObject, ObservableObject {
     }
     
     func connect() {
-        guard let peripheral = selectedPeripheral else { return }
-        centralManager?.connect(peripheral, options: nil)
+        if let peripheral = selectedPeripheral {
+            centralManager?.connect(peripheral, options: nil)
+        }
     }
     
     func disconnect() {
         guard let peripheral = selectedPeripheral else { return }
         centralManager?.cancelPeripheralConnection(peripheral)
     }
+    
 }
 
 extension BluetoothViewModel: CBCentralManagerDelegate {
@@ -39,11 +41,14 @@ extension BluetoothViewModel: CBCentralManagerDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        if !peripherals.contains(peripheral) {
-            self.peripherals.append(peripheral)
-            self.peripheralNames.append(peripheral.name ?? "unnamed device")
+        if let peripheralName = peripheral.name {
+            if !peripheralNames.contains(peripheralName) {
+                self.peripherals.append(peripheral)
+                self.peripheralNames.append(peripheralName)
+            }
         }
     }
+
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         self.connectionStatus = "Conexión Exitosa"
@@ -51,23 +56,34 @@ extension BluetoothViewModel: CBCentralManagerDelegate {
     }
     
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
-        self.connectionStatus = "No se completó la Conexión"
+        if let error = error {
+            self.connectionStatus = "No se completó la Conexión: \(error.localizedDescription)"
+        } else {
+            self.connectionStatus = "No se completó la Conexión"
+        }
         print("No jaló la conexión")
     }
-    
+
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-        self.connectionStatus = "Desconectado"
+        if let error = error {
+            self.connectionStatus = "Desconectado: \(error.localizedDescription)"
+        } else {
+            self.connectionStatus = "Desconectado"
+        }
         print("Desconectado")
     }
+
 }
 
 struct Bluetooth_Desvergue: View {
     @ObservedObject private var bluetoothViewModel = BluetoothViewModel()
+    private var characteristic: CBCharacteristic? = nil
+    @State private var receivedMessage: String? = nil
     
     var body: some View {
         NavigationView {
             VStack {
-                List(bluetoothViewModel.peripheralNames, id: \.self) { peripheralName in
+                List(Array(bluetoothViewModel.peripheralNames), id: \.self) { peripheralName in
                     Button(action: {
                         if let index = bluetoothViewModel.peripheralNames.firstIndex(of: peripheralName) {
                             bluetoothViewModel.selectedPeripheral = bluetoothViewModel.peripherals[index]
@@ -83,6 +99,16 @@ struct Bluetooth_Desvergue: View {
                     bluetoothViewModel.disconnect()
                 }).offset(x:-100).padding()
                 Text(bluetoothViewModel.connectionStatus).offset(x:100, y:-45)
+                
+                if let selectedPeripheral = bluetoothViewModel.selectedPeripheral {
+                    VStack {
+                        Button("Enviar mensaje") {
+                            let data = "Hola desde el celular".data(using: .utf8)!
+                            selectedPeripheral.writeValue(data, for: characteristic!, type: .withoutResponse)
+                        }
+                        Text("Mensaje recibido: \(receivedMessage ?? "N/A")")
+                    }
+                }
             }
         }
         .onAppear() {
@@ -93,6 +119,7 @@ struct Bluetooth_Desvergue: View {
         }
     }
 }
+
 
 struct Bluetooth_Desvergue_Previews: PreviewProvider {
     static var previews: some View {
